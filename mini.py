7ba8197fb2786434fbf2c642e53c076a7e89a133
Prashanthcoder <tp120113@gmail.com>
2025-07-24 21:01:01 +0530

    tried to check origin cors issue

--- mini.py
+++ mini.py
@@ -8,7 +8,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  # or ["http://127.0.0.1:5500"]
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
